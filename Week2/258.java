222ef09e7067eab481a7d73dc5190ec1c3745ce3
Pku <pkusser@sina.cn>
2015-10-10 12:38:33 +0800

    258

---
+++ Week2/258.java
@@ -0,0 +1,12 @@
+//可以用数字根公式
+//dr(n)=1+((n-1) mod 9)
+//https://en.wikipedia.org/wiki/Digital_root
+//时间复杂度O(1)
+public class Solution {
+    public int addDigits(int num) {
+        if(num<9)
+			return num;
+        int result= num%9;
+        return 0==result?9:result;
+    }
+}
